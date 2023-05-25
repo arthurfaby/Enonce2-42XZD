2c36f91482c28ce560c5961e332b85230dfe574e
Arthur Faby <arthfaby33@gmail.com>
2023-05-25 12:28:54 +0200

    Create plugin file and add it to wordpress. Prepare chatbot form and connection to database.

---
+++ plugin/zinzin-chatbotté/bot.php
@@ -0,0 +1,33 @@
+<?php /**
+* Plugin Name: ZinzinChatBotté
+* Plugin URI: http://localhost/
+* Description: Test.
+* Version: 0.1
+* Author: toto
+* Author URI: http://localhost/
+**/
+
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
+add_action('wp_head', 'our_pirabaud');
+
+function our_pirabaud() {
+
+    ?>
+    <form>
+        <input type="text" placeholder="Parlez à ChatBotté" id="message" name="message">
+        <input type="submit" value="Envoyer">
+    </form>
+    <?php
+    // global $wpdb;
+    // $query = $wpdb->prepare("SELECT * FROM wp_posts;");
+    // $rows = $wpdb->get_row( $query );
+
+    // print_r($rows);
+
+    if (empty($_GET['message'])) {
+        echo "<h2>No message</h2>";
+    } else {
+        $message = $_GET['message'];
+        echo "<h2>$message</h2>";
+    };
+}
